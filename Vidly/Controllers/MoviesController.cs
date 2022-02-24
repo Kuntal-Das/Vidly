d3c07d9b5faa0c8dcc4f8cb91102902024fd8290
Kuntal Das <25ckuntaldas@gmail.com>
2022-02-24 13:29:34 +0530

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //GET: Movies/Random
+        public IActionResult Random()
+        {
+            Movie movie = new() { Name = "Dune 2021" };
+
+            return View(movie);
+        }
+    }
+}
