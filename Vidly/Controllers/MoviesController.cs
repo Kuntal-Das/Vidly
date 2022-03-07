39200f8833e64dc4775f189a66068f717f574bb3
Kuntal Das <25ckuntaldas@gmail.com>
2022-03-07 12:11:58 +0530

    Action Results

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,12 +9,30 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        //Action Results:
+        // ViewResult           -> View() 
+        // PartialViewResult    -> PartialView()
+        // ContentResult        -> Content()
+        // RedirectResult       -> Redirect()
+        // RedirectRouteResult  -> RedirectToAction()
+        // JsonResult           -> Json()
+        // FileResult           -> File()
+        // HttpNotFoundResult   -> HttpNotFound() or NotFound()
+        // EmptyResult          
+
         //GET: Movies/Random
         public IActionResult Random()
         {
             Movie movie = new() { Name = "Dune 2021" };
 
-            return View(movie);
+            //return View(movie);
+            //return new ViewResult();
+            //return Content("Hellow World!");
+            //return NotFound();
+
+            //return new EmptyResult();
+
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
     }
 }
