581fa3384465a85b113dc898c50c3342d5cbd476
Kuntal Das <25ckuntaldas@gmail.com>
2022-03-09 12:26:38 +0530

    DB migration :AddIsCustomerSubscribedToNewsLetter

---
+++ Vidly/Data/Migrations/20220309065412_AddIsCustomerSubscribedToNewsLetter.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Data.Migrations
+{
+    public partial class AddIsCustomerSubscribedToNewsLetter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSubscribedToNewsLetter",
+                table: "Customers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSubscribedToNewsLetter",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
