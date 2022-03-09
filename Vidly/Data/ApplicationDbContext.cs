04ae4663930762b9ac9bc241ae6ca60a785ef60e
Kuntal Das <25ckuntaldas@gmail.com>
2022-03-09 12:19:10 +0530

    migration : initial model

--- Vidly/Data/ApplicationDbContext.cs
+++ Vidly/Data/ApplicationDbContext.cs
@@ -3,11 +3,15 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Vidly.Models;
 
 namespace Vidly.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
