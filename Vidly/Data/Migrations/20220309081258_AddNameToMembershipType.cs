41b7cc7faa593f7b0158954724b761eea2b8a393
Kuntal Das <25ckuntaldas@gmail.com>
2022-03-10 10:25:34 +0530

    populate names in membership

---
+++ Vidly/Data/Migrations/20220309081258_AddNameToMembershipType.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Data.Migrations
+{
+    public partial class AddNameToMembershipType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "MembershipType",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "MembershipType");
+        }
+    }
+}
---
+++ Vidly/Data/Migrations/20220309081604_PopulateNamesInMembershipType.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Data.Migrations
+{
+    public partial class PopulateNamesInMembershipType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE MEMBERSHIPTYPE SET Name='Pay As you go' WHERE Id=1");
+            migrationBuilder.Sql("UPDATE MEMBERSHIPTYPE SET Name='Monthly' WHERE Id=2");
+            migrationBuilder.Sql("UPDATE MEMBERSHIPTYPE SET Name='Quaterly' WHERE Id=3");
+            migrationBuilder.Sql("UPDATE MEMBERSHIPTYPE SET Name='Yearly' WHERE Id=4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
