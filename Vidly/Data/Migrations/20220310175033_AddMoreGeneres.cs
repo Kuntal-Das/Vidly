beaf534b330542d7b31bc340a779ac7d86230198
Kuntal Das <25ckuntaldas@gmail.com>
2022-03-10 23:25:26 +0530

    more grneres added

---
+++ Vidly/Data/Migrations/20220310175033_AddMoreGeneres.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Data.Migrations
+{
+    public partial class AddMoreGeneres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.Sql("INSERT INTO GENERE (Id, Name) VALUES (1, 'Action')");
+            //migrationBuilder.Sql("INSERT INTO GENERE (Id, Name) VALUES (2, 'Thriller')");
+            //migrationBuilder.Sql("INSERT INTO GENERE (Id, Name) VALUES (3, 'Family')");
+            //migrationBuilder.Sql("INSERT INTO GENERE (Id, Name) VALUES (4, 'Romance')");
+            //migrationBuilder.Sql("INSERT INTO GENERE (Id, Name) VALUES (5, 'Comedy')");
+            migrationBuilder.Sql("INSERT INTO GENERE (Id, Name) VALUES (6, 'Sci-fi')");
+            migrationBuilder.Sql("INSERT INTO GENERE (Id, Name) VALUES (7, 'Adventure')");
+            migrationBuilder.Sql("INSERT INTO GENERE (Id, Name) VALUES (8, 'Sci-fi/Adventure')");
+            migrationBuilder.Sql("INSERT INTO GENERE (Id, Name) VALUES (9, 'Sci-fi/Thriller')");
+            migrationBuilder.Sql("INSERT INTO GENERE (Id, Name) VALUES (10, 'Comedy/Romance')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
