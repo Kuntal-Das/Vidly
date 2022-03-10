1fec537fc6bb7e5988410d22ec96c5f2fd208a10
Kuntal Das <25ckuntaldas@gmail.com>
2022-03-10 11:36:27 +0530

    migration : AddDOBToCustomer

---
+++ Vidly/Data/Migrations/20220310060410_AddDOBToCustomer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Data.Migrations
+{
+    public partial class AddDOBToCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DOB",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DOB",
+                table: "Customers");
+        }
+    }
+}
